fa7da282131cec27223a5d061e7a87947c3f9822
kamleshkumar <kamlesh.kumar@wibmo.com>
2024-03-04 15:20:20 +0530

    Initial commit

---
+++ DataStructureWithSwift/App/DataStructureWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructureWithSwiftApp.swift
+//  DataStructureWithSwift
+//
+//  Created by Kamalesh Kumar on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DataStructureWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
